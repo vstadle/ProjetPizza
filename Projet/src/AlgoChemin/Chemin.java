9a8f54e5495ac191d480f187d942dd97e0ec34f1
STADLER Valentin <valentin.stadler@universite-paris-saclay.fr>
2023-11-27 09:51:50 +0100

    V1 Sans Maven

---
+++ Projet/src/AlgoChemin/Chemin.java
@@ -0,0 +1,63 @@
+package AlgoChemin;
+
+import java.util.ArrayList;
+
+public class Chemin {
+	int TEMPSKM = 5;
+	
+	//Liste des addresses
+	ArrayList<Position> chemin;
+	//ArrayList<Pizza> pizzaenPrep = new ArrayList<Position>();
+		
+	//Critères de livraison ratio Temps/Trajet
+	double tempsLivraison;
+	int nbPizza;
+	double distanceLivraison;
+	
+	public Chemin () {
+		this.nbPizza = 0;
+		this.distanceLivraison = 0;
+		this.tempsLivraison = 15;
+		this.chemin = new ArrayList<>();
+	}
+	
+	public Chemin (ArrayList<Position> adresse) {
+		this.chemin = adresse;
+		this.nbPizza = adresse.size();
+		this.tempsLivraison = this.calculTemps() + 15;
+		this.distanceLivraison = this.calculDistance();
+	}
+	
+	//On cherche à calculer le ration Temps/Distance
+	public double rationTempsPizza() {
+		return this.tempsLivraison/this.distanceLivraison;
+	}
+	
+	public double calculDistance() {
+		double distance = 0;
+		for(int i=0 ; i<this.chemin.size();i++) {
+			distance = distance + this.chemin.get(i).distance(this.chemin.get(i+1));
+		}
+		return distance;
+	}
+	
+	public double calculTemps() {
+		return this.distanceLivraison*TEMPSKM;
+	}
+	
+	public Chemin calculCheminDyna(ArrayList<Position> listeAdresse) {
+		Chemin temps = new Chemin(listeAdresse);
+		Chemin c = new Chemin();
+		int cpt = 0;
+		try {
+			if(this.rationTempsPizza()<c.rationTempsPizza()) {
+				temps.chemin.remove(cpt);
+				//this.
+			}
+		}
+		catch(Exception e){
+		
+		}
+		return new Chemin();
+	}
+}
---
+++ Projet/src/AlgoChemin/Position.java
@@ -0,0 +1,109 @@
+package AlgoChemin;
+
+import java.util.ArrayList;
+
+public class Position {
+	private double x;
+	private double y;
+	
+	public double getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public double getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public Position(int x, int y)
+	{
+		this.x=x;
+		this.y=y;
+	}
+	
+	public Position (String x, String y) {
+		this.x = Double.parseDouble(x);
+		this.y = Double.parseDouble(y);
+	}
+	
+	public String toString() {
+		String temp = "lat : " + this.getY() + " long: " + this.getX();
+		return temp;
+	}
+	
+	public double distance(Position p)
+	{
+		return Math.sqrt((this.getX()-p.getX())*(this.getX()-p.getX())+(this.getY()-p.getY())*(this.getY()-p.getY()));
+	}
+	
+	//renvoie Ã  partir d'une ArrayList de positions une ArrayList de positions triÃ©e dans l'ordre des postions Ã  parcourir pour avoir le chemin le plus optimale
+	public static ArrayList<Position> cheminOptiGlouton(ArrayList<Position> positions, Position depart) 
+	{
+		ArrayList<Position> chemin = new ArrayList<Position>();
+		chemin.add(depart);
+		if (positions.size()==0)
+		{
+			return chemin;
+		}
+		else
+		{
+			double distMax = 0;
+			int indMax = 0;
+			for(int i=0; i<positions.size(); i++)
+			{
+				if(depart.distance(positions.get(i))>distMax) 
+				{
+					indMax= i;
+					distMax = depart.distance(positions.get(i));
+				}
+			}
+			Position etapeSuivante = positions.get(indMax);
+			positions.remove(indMax);
+			chemin = cheminOptiGlouton(positions, depart);	
+			chemin.add(etapeSuivante);
+			return chemin;
+		}
+	}
+	
+	//(0;0)->(5;8)->(1;9)->(3;7)->(2;6)->(3;4)->
+	public static int sommeDist(ArrayList<Position> positions)
+	{
+		int somme = 0;
+		for(int i=0; i<positions.size()-1; i++)
+		{
+			somme+= positions.get(i).distance(positions.get(i+1));
+		}
+		return somme;		
+	}
+	
+	
+	public static void afficherPositions(ArrayList<Position> positions)
+	{
+		String s = "";
+		for(Position p : positions)
+		{
+			s+= "("+p.x+";"+p.y+")->";
+		}
+		System.out.println(s);
+	}
+	
+	public static void main(String[] args) {
+		ArrayList<Position> positions = new ArrayList<Position>();
+		positions.add(new Position(3,4));
+		positions.add(new Position(1,9));
+		positions.add(new Position(2,6));
+		positions.add(new Position(5,8));
+		positions.add(new Position(3,7));
+		afficherPositions(positions);
+		Position depart = new Position(0,0);
+		//ArrayList<Position> chemin = cheminOpti(positions,depart);
+		//afficherPositions(chemin);
+		System.out.println(depart.distance(new Position(5,8)));
+		System.out.println(depart.distance(new Position(3,4)));
+	}
+	
+}
